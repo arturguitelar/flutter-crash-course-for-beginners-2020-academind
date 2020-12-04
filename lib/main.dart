5730b9bd39d97d447c9c6664dd23061b928dd2af
Artur Guitelar <artur.guitelar@gmail.com>
2020-12-04 03:36:33 -0300

    Criado alguns elementos para ilustrar a interface: Bassa de título, texto e botões no body.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+// // main padrão
+// void main() {
+//   runApp(MyApp());
+// }
+// Se a função tem apenas uma expressão é possível usar
+// uma espécie de "Arrow Function" (Como no javascript).
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
     return MaterialApp(
-      home: Text('Olá'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Um titulo para o App aqui'),
+        ),
+        body: Column(
+          children: [
+            Text('A pergunta!'),
+            RaisedButton(
+              child: Text('Pergunta 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Pergunta 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Pergunta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

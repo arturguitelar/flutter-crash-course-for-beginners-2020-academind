4b9ce62017dc52bfe5415b926917e6e4a87adac9
Killdragon <artur.guitelar@gmail.com>
2021-01-01 12:26:21 -0300

    ⚙️refactor: Adiciona texto de 'Você conseguiu'
    - Adiciona texto de 'Você conseguiu' à tela caso as questões acabem.

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,39 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      'question': 'Qual é a sua cor favorita?',
+      'answers': ['Preto', 'Vermelho', 'Verde', 'Azul', 'Branco']
+    },
+    {
+      'question': 'Qual é o seu animal favorito?',
+      'answers': ['Cachorro', 'Gato', 'Leão', 'Tigre', 'Cavalo', 'Galinha']
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'question': 'Qual é a sua cor favorita?',
-        'answers': ['Preto', 'Vermelho', 'Verde', 'Azul', 'Branco']
-      },
-      {
-        'question': 'Qual é o seu animal favorito?',
-        'answers': ['Cachorro', 'Gato', 'Leão', 'Tigre', 'Cavalo', 'Galinha']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Um titulo para o App aqui'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['question']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) => Answer(answer, _answerQuestion))
-                .toList(),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: _questionIndex < questions.length
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Question(questions[_questionIndex]['question']),
+                    ...(questions[_questionIndex]['answers'] as List<String>)
+                        .map((answer) => Answer(answer, _answerQuestion))
+                        .toList(),
+                  ],
+                )
+              : Center(
+                  child: Text('Você conseguiu! :D'),
+                ),
         ),
       ),
     );
@@ -48,5 +57,10 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex += 1;
     });
+    if (_questionIndex < questions.length) {
+      print('we have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 }
